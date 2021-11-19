06e6eec4a745415e8c2b7d15af57a6c8ba7ac887
mobilex1122 <mobilex1122.en@gmail.com>
2021-11-19 16:36:14 +0100

    menu update

--- assets/menu/level.gd
+++ assets/menu/level.gd
@@ -1,25 +1,19 @@
-extends TextureButton
+extends Button
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var size_max = 3
-export var rotate = 1
 
 
-var pos_incrase_calc = Vector2(0,0)
-var i = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-	
-func _process(delta):
-	if is_hovered():
-		modulate = Color(0.9,0.9,0.9)
-	else:
-		modulate = Color(1,1,1)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+
+func _on_level_pressed():
+	get_tree().change_scene("res://levels/"+text+".tscn")
---
+++ assets/menu/menu_anim.gd
@@ -0,0 +1,30 @@
+extends AnimationPlayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var open = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play("reset")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_play_pressed():
+	if open == 0 and not is_playing():
+		play("levels-open")
+		open = 1
+	elif open == 1 and not is_playing():
+		play_backwards("levels-open")
+		open = 0
+
+
+func _on_infinite_pressed():
+	if not is_playing():
+		play("infinite")
