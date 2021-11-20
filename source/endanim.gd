ca8a2b835960fd7cec651fb6fbdf42d370ef8dad
mobilex1122 <mobilex1122.en@gmail.com>
2021-11-20 12:09:40 +0100

    level and animation update

---
+++ source/endanim.gd
@@ -0,0 +1,24 @@
+extends AnimationPlayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var played = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
+
+func _on_Area2D_body_entered(body):
+		if played == 0:
+			played = 1
+			play("end")
